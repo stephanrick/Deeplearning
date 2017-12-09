6b606da85d7190220752805ce5d0c48a12c7a0c6
stephanrick <34286123+stephanrick@users.noreply.github.com>
2017-12-09 20:04:38 +0100

    Update classify.py

--- classify.py
+++ classify.py
@@ -8,7 +8,6 @@ def NBAccuracy(features_train, labels_train, features_test, labels_test):
     
 
     ### fit the classifier on the training features and labels
-    #TODO
     clf.fit(features_train, labels_train)
 
     ### use the trained classifier to predict labels for the test features
@@ -18,7 +17,6 @@ def NBAccuracy(features_train, labels_train, features_test, labels_test):
     ### calculate and return the accuracy on the test data
     ### this is slightly different than the example, 
     ### where we just print the accuracy
-    ### you might need to import an sklearn module
     from sklearn.metrics import accuracy_score
     print accuracy_score(pred, labels_test)
     accuracy = accuracy_score(pred, labels_test)
